d76959b351d76e7abdb1c7e77a76f7ee3cbb2914
Steven McConomy <srmcconomy@gmail.com>
2016-06-12 12:33:21 -0400

    ^

---
+++ a1/removecontent.cpp
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+#include "core.cpp"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (connectToPeer(sockfd, argv[1], atoi(argv[2])) < 0)
+		printf("Error: no such peer");
+
+	string command = "removecontent ";
+	command.append(argv[3]);
+	sendMessage(sockfd, command);
+
+	if(recieveMessage(sockfd) != "done")
+		printf("Error: no such peer");
+
+	return 0;
+}
