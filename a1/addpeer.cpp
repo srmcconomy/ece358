47548613b1f1fa916e6d83d9de7140cff8cd20d8
1habeebahmed1 <1habeebahmed1@gmail.com>
2016-06-13 22:36:59 -0400

    done forking

--- a1/addpeer.cpp
+++ a1/addpeer.cpp
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <iostream>
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -41,110 +42,132 @@ int main(int argc, char* argv[]) {
   if (getsockname(sockfd, (struct sockaddr *)&addr, &alen) < 0) {
     perror("getsockname"); return -1;
   }
-  //if (fork()) {
-  vector<peer> peers;
-  map<unsigned int, string> content;
-  unsigned int last_content_id = 1;
+  // setup messaging between the child and parent
+  int pipefd[2];
+  pipe(pipefd); 
+  // fork child process
+  int pid = fork();
+  if (pid == 0) {
+    // child process
+    vector<peer> peers;
+    map<unsigned int, string> content;
+    unsigned int last_content_id = 1;
 
-  peer self = {
-    inet_ntoa(addr.sin_addr),
-    ntohs(addr.sin_port),
-    0
-  };
-  // add self to list of peers
-  peers.push_back(self);
+    peer self = {
+      inet_ntoa(addr.sin_addr),
+      ntohs(addr.sin_port),
+      0
+    };
+    // add self to list of peers
+    peers.push_back(self);
 
-  if (argc == 3) {
-    // if a ip and port of an existing network was specified then sync with that network
-    int socktd = socket(AF_INET, SOCK_STREAM, 0);
+    if (argc == 3) {
+      // if a ip and port of an existing network was specified then sync with that network
+      int socktd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // String-format: "newpeer 172.46.14.3 10013"
-    string cmd = "newpeer ";
-    cmd.append(inet_ntoa(addr.sin_addr));
+      // String-format: "newpeer 172.46.14.3 10013"
+      string cmd = "newpeer ";
+      cmd.append(inet_ntoa(addr.sin_addr));
+      cmd.append(" ");
+      cmd.append(int_to_string(ntohs(addr.sin_port)));
+
+      connectToPeer(socktd, argv[1], atoi(argv[2]));
+      sendMessage(socktd, cmd);
+      istringstream iss(recieveMessage(socktd));
+      int peerCount;
+      iss >> peerCount;
+
+      for (int i = 0; i < peerCount; i++) {
+        peer dao = get_peer(iss);
+        peers.push_back(dao);
+      }
+
+      printf("%s\n", list_of_peers(peers).c_str());
+
+      shutdown(socktd, SHUT_RDWR);
+
+    }
+
+    listen(sockfd, 10);
+
+    // request content from other peers in order to balance out content
+    int totalContent = 0;
+
+    for (int i = 0 ; i < peers.size() ; i++) {
+      totalContent += peers[i].numContent;
+    }
+    int contentNeeded = floor(totalContent / (double)peers.size());
+    while(contentNeeded > 0) {
+      // take one content from the peer with the most content
+      int biggestContentPeer = 0;
+      for (int i = 1 ; i < peers.size() ; i++) {
+          if (peers[biggestContentPeer].numContent <= peers[i].numContent) {
+            biggestContentPeer = i;
+          }
+      }
+
+      int sockid = socket(AF_INET, SOCK_STREAM, 0);
+      connectToPeer(sockid, peers[biggestContentPeer].ip, peers[biggestContentPeer].port);
+      sendMessage(sockid, "needcontent");
+
+      //handle the other peer's "numcontent" broadcast
+      handle_message(sockfd, peers, content, last_content_id);
+
+      unsigned int newid;
+      string newcontent;
+      istringstream newss(recieveMessage(sockid));
+      // add content into this peer
+      newss >> newid;
+      getline(newss, newcontent);
+      newcontent = newcontent.substr(1);
+      content[newid] = newcontent;
+      // increment the ammount of content in this peer
+      peers[0].numContent++;
+      sendMessage(sockid, "done");
+      close(sockid);
+      --contentNeeded;
+    }
+    
+    //tell everyone I have a different amount of content now
+    string cmd = "numcontent ";
+    cmd.append(peers[0].ip);
     cmd.append(" ");
-    cmd.append(int_to_string(ntohs(addr.sin_port)));
-
-    connectToPeer(socktd, argv[1], atoi(argv[2]));
-    sendMessage(socktd, cmd);
-    istringstream iss(recieveMessage(socktd));
-    int peerCount;
-    iss >> peerCount;
-
-    for (int i = 0; i < peerCount; i++) {
-      peer dao = get_peer(iss);
-      peers.push_back(dao);
+    cmd.append(int_to_string(peers[0].port));
+    cmd.append(" ");
+    cmd.append(int_to_string(peers[0].numContent));
+    for (int i = 1; i < peers.size(); i++) {
+      int sockid = socket(AF_INET, SOCK_STREAM, 0);
+      connectToPeer(sockid, peers[i].ip, peers[i].port);
+      sendMessage(sockid, cmd);
+      recieveMessage(sockid);
+      close(sockid);
     }
+    // finished load balancing on add peer
+    cout << inet_ntoa(addr.sin_addr) << " " << ntohs(addr.sin_port) << endl;
 
-    printf("%s\n", list_of_peers(peers).c_str());
-
-    shutdown(socktd, SHUT_RDWR);
-
-  }
-
-  listen(sockfd, 10);
-
-  // request content from other peers in order to balance out content
-  int totalContent = 0;
-
-  for (int i = 0 ; i < peers.size() ; i++) {
-    totalContent += peers[i].numContent;
-  }
-  int contentNeeded = floor(totalContent / (double)peers.size());
-  while(contentNeeded > 0) {
-    // take one content from the peer with the most content
-    int biggestContentPeer = 0;
-    for (int i = 1 ; i < peers.size() ; i++) {
-        if (peers[biggestContentPeer].numContent <= peers[i].numContent) {
-          biggestContentPeer = i;
-        }
+    // we are done syncing with the network. 
+    // notify the parent to terminate
+    cout<<"finished syncing with network"<<endl;
+    close(pipefd[0]); // close the read-end of the pipe, child does not use it
+    write(pipefd[1], "x", 1); // send some char to the parent
+    close(pipefd[1]); // close the write-end of the pipe, thus sending EOF to the reader
+    // Infinite listen loop
+    for (;;) {
+      int retCode = handle_message(sockfd, peers, content, last_content_id);
+      if (retCode == RETURN_ERROR || retCode == RETURN_SHUTDOWN) break;
     }
-
-    int sockid = socket(AF_INET, SOCK_STREAM, 0);
-    connectToPeer(sockid, peers[biggestContentPeer].ip, peers[biggestContentPeer].port);
-    sendMessage(sockid, "needcontent");
-
-    //handle the other peer's "numcontent" broadcast
-    handle_message(sockfd, peers, content, last_content_id);
-
-    unsigned int newid;
-    string newcontent;
-    istringstream newss(recieveMessage(sockid));
-    // add content into this peer
-    newss >> newid;
-    getline(newss, newcontent);
-    newcontent = newcontent.substr(1);
-    content[newid] = newcontent;
-    // increment the ammount of content in this peer
-    peers[0].numContent++;
-    sendMessage(sockid, "done");
-    close(sockid);
-    --contentNeeded;
+  } else if(pid > 0){
+    // parent process
+    close(pipefd[1]); // close the write-end of the pipe, parent does not use it
+    char buf;
+    while (read(pipefd[0], &buf, 1) > 0) {
+      if(buf == 'x') break;
+    }
+    cout<<"parent notified"<<endl;
+    close(pipefd[0]); // close the read-end of the pipe
+  } else {
+    // fork failed
+    cout<<"Fork failed!"<<endl;
   }
-  
-  //tell everyone I have a different amount of content now
-  string cmd = "numcontent ";
-  cmd.append(peers[0].ip);
-  cmd.append(" ");
-  cmd.append(int_to_string(peers[0].port));
-  cmd.append(" ");
-  cmd.append(int_to_string(peers[0].numContent));
-  for (int i = 1; i < peers.size(); i++) {
-    int sockid = socket(AF_INET, SOCK_STREAM, 0);
-    connectToPeer(sockid, peers[i].ip, peers[i].port);
-    sendMessage(sockid, cmd);
-    recieveMessage(sockid);
-    close(sockid);
-  }
-  // finished load balancing on add peer
-  cout << inet_ntoa(addr.sin_addr) << " " << ntohs(addr.sin_port) << endl;
-
-  // Infinite listen loop
-  for (;;) {
-    int retCode = handle_message(sockfd, peers, content, last_content_id);
-    if (retCode == RETURN_ERROR || retCode == RETURN_SHUTDOWN) break;
-  }
-//} else {
-
-//}
   return 0;
 }
